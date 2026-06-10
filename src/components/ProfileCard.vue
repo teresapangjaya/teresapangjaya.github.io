cb46aabf109caff9230bf6d283e63ac4e5818a19
Heru Trijaya <elniteru@gmail.com>
2026-06-10 19:53:41 +0800

    change avatar

--- src/components/ProfileCard.vue
+++ src/components/ProfileCard.vue
@@ -3,12 +3,7 @@
 
 <template>
   <div class="avatar-wrap">
-    <div class="avatar-placeholder">
-      <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5">
-        <circle cx="12" cy="8" r="4"/><path d="M4 20c0-4 3.6-7 8-7s8 3 8 7"/>
-      </svg>
-      <span>Photo</span>
-    </div>
+    <img src="/img.png" alt="Teresa Pangjaya" />
   </div>
   <div class="profile-name">Teresa Pangjaya</div>
   <div class="profile-title">QA Testing | Agile Software Development<br>Agile Project Management</div>
